e943cd327a3e297e4edcc0467d54ea48c1cc34bd
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-07-07 20:24:14 -0600

    made the lasers shoot in a short uniform bursts!

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include "game.h"
+#include <iostream>
 
 Game::Game() {
 
@@ -11,6 +12,9 @@ void Game::Update() {
 	for (auto& laser : spaceship.lasers) {
 		laser.Update();
 	}
+
+	DeleteInactiveLasers(); // Calling the function defined below
+	std::cout << "Vector Size: " << spaceship.lasers.size() << std::endl;
 }
 
 void Game::Draw() {
@@ -36,5 +40,12 @@ void Game::HandleInput() {
 
 void Game::DeleteInactiveLasers()
 {
-
+	for (auto it = spaceship.lasers.begin(); it != spaceship.lasers.end();) { //Created an 'it' vector and an iterator to go through it.
+		if (!it->active) {						//The iterator go through and sees if any of the lasers are active or not.  
+			it = spaceship.lasers.erase(it); //If is any inactive lasers, it gets removed
+		}
+		else { // if the element is active, iterator moves to the next element
+			++it;
+		}
+	}
 }
