5fd85be772731982a93ed84661044b332b70fa28
Alwin Rajkumar <141786788+alwnraj@users.noreply.github.com>
2024-07-13 14:39:01 -0400

    made some more objects and started with coding blocks

--- obstacle.cpp
+++ obstacle.cpp
@@ -4,7 +4,7 @@ Obstacle::Obstacle(Vector2 position)
 {
 	this->position = position;
 
-    grid = {
+    grid = { // This grid will serve as a reference for the 'block' 
     {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
     {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
     {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
@@ -20,9 +20,14 @@ Obstacle::Obstacle(Vector2 position)
     {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1}
     };
 
-    for (unsigned int row = 0; row < grid.size(); ++row) {
-        for (unsigned coloumn = 0; coloumn < grid[0].size(); ++coloumn) {
-
+    for (unsigned int row = 0; row < grid.size(); ++row) { // basically represents each row in the grid, iterates through each of them
+        for (unsigned column = 0; column < grid[0].size(); ++column) { //iterates through each coloumn in the current row, 
+            //Note: grid[0] refers to the first row in the grid, so grid[0].size gives us the no. of columns
+            if (grid[row][column] == 1) {
+                float pos_x = position.x + column*3; 
+                float pos_y = position.y + row*3;// The block is three pixels tall, so we are multiplying by 3 to take into account for that.
+                Block block = Block({ pos_x, pos_y });
+            }
         }
     }
 }
--- obstacle.h
+++ obstacle.h
@@ -7,6 +7,9 @@ class Obstacle
 public:
 	Obstacle(Vector2 position);
 	void Draw();
+	Vector2 position;
+	std::vector<Block> blocks;
+	std::vector<std::vector<int>> grid;
 
 private:
 
